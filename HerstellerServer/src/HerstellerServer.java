f58af40186141c3f47ee550695fda311b65944d2
SeloPeylo <m.selim.sinan@gmail.com>
2019-01-16 14:32:03 +0100

    mqtt library eingefügt

---
+++ HerstellerServer/src/HerstellerServer.java
@@ -0,0 +1,10 @@
+import org.eclipse.paho.client.mqttv3.*;
+
+public class HerstellerServer {
+
+
+    public void main(String[] args)
+    {
+
+    }
+}
--- HerstellerServer/src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-
-
-    public void main(String[] args)
-    {
-
-    }
-}
