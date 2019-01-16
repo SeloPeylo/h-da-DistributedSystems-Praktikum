76d35ae48fce6a4385b342c99900cbc40751f43a
SeloPeylo <m.selim.sinan@gmail.com>
2019-01-16 15:04:44 +0100

    fixed mqtt library

--- HerstellerServer/src/HerstellerServer.java
+++ HerstellerServer/src/HerstellerServer.java
@@ -1,10 +1,32 @@
 import org.eclipse.paho.client.mqttv3.*;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HerstellerServer {
 
 
-    public void main(String[] args)
+    public void main(String[] args) throws MqttException
     {
 
+        MqttClient client=new MqttClient("tcp://localhost:1883", MqttClient.generateClientId());
+        client.setCallback( new SimpleMqttCallBack() );
+        client.connect();
+
+
+    }
+
+    public class SimpleMqttCallBack implements MqttCallback {
+
+        public void connectionLost(Throwable throwable) {
+            System.out.println("Connection to MQTT broker lost!");
+        }
+
+        public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+            System.out.println("Message received:\n\t"+ new String(mqttMessage.getPayload()) );
+        }
+
+        public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+            // not used in this example
+        }
     }
 }
